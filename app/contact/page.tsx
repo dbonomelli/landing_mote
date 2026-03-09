4118dffe3f02b9486406de843bd57b27d82f799e
Diego Muñoz Del Pino <61483941+dbonomelli@users.noreply.github.com>
2026-03-09 02:36:47 -0300

    advances

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,21 @@
 import { JSX } from "react";
+import Card from "@/components/Card/Card";
 
 export default function Contact(): JSX.Element {
     return (
-        <div>D</div>
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                <Card
+                    title="John Doe"
+                    description="+56 9 1234 5678"
+                    price=""
+                />
+                <Card
+                    title="Jane Smith"
+                    description="+56 9 8765 4321"
+                    price=""
+                />
+            </div>
+        </div>
     );
 }
--- app/prices/page.tsx
+++ app/prices/page.tsx
@@ -1,23 +1,50 @@
+"use client";
 import Card from "@/components/Card/Card";
 import { JSX } from "react";
+import { motion } from "framer-motion";
 
 export default function Prices(): JSX.Element {
+  const cardVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 1.0 } },
+  };
+
+  const hoverEffect = {
+    scale: 1.02, // Slightly increase the size
+    boxShadow: "0px 4px 15px rgba(0, 0, 0, 0.2)", // Add a shadow effect
+  };
+
   return (
     <div className="flex flex-col gap-2 p-8">
-      <div>
-        <h1 className="text-3xl font-bold mb-2">Precios en carro</h1>
-        <p>Puede ser vasos de vidrio o de plástico.</p>
+      <div className="flex flex-row sm:flex-col">
+        <h1 className="text-3xl font-bold">Precios en carro</h1>
+        <p className="text-sm self-center">Puede ser vasos de vidrio o de plástico.</p>
       </div>
       <div className="space-y-6">
-        <Card title="Mote Simple" price="$1.800" description="Mote con huesillo de xx lt"/>
-        <Card title="Mote Mediano" price="$2.300" description="Mote con huesillo de xx lt"/>
-        <Card title="Mote Grande" price="$3.300" description="Mote con huesillo de xx lt"/>
-      </div>
-      <h1 className="text-3xl font-bold mb-2">Precio por mayor</h1>
-      <div className="space-y-6">
-        <Card title="Mote Simple" price="$1.800" description="Mote con huesillo de xx lt"/>
-        <Card title="Mote Mediano" price="$2.300" description="Mote con huesillo de xx lt"/>
-        <Card title="Mote Grande" price="$3.300" description="Mote con huesillo de xx lt"/>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={cardVariants}
+          whileHover={hoverEffect}
+        >
+          <Card title="Mote Simple" price="$1.800" description="Mote con huesillo de xx lt" />
+        </motion.div>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={cardVariants}
+          whileHover={hoverEffect}
+        >
+          <Card title="Mote Mediano" price="$2.300" description="Mote con huesillo de xx lt" />
+        </motion.div>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={cardVariants}
+          whileHover={hoverEffect}
+        >
+          <Card title="Mote Grande" price="$3.300" description="Mote con huesillo de xx lt" />
+        </motion.div>
       </div>
     </div>
   );
